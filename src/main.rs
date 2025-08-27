cf34c685c8c5ae010333644969bf3319b64553a3
NJ <226465585+nj-twice@users.noreply.github.com>
2025-08-27 20:50:54 +0200

    Make sure there's only one battery

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,8 @@
-fn main() {}
+use battery;
+
+fn main() {
+    let manager = battery::Manager::new().unwrap();
+
+    // Assume this is a laptop and there's only one single battery
+    assert_eq!(manager.batteries().iter().count(), 1)
+}
