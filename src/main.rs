b2d87b508b93094a21abf08e9a97ce8d67fabbc7
NJ <226465585+nj-twice@users.noreply.github.com>
2025-08-28 20:02:47 +0200

    Assume one screen and update brightness

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,23 @@
+use brightness::blocking::{self as bb, Brightness};
 use std::{thread, time::Duration};
 
 const THREE_MINUTES: u64 = 180;
 const THIRTY_PERCENT: f32 = 0.3;
 const NINETY_PERCENT: f32 = 0.9;
 const ONE_SECOND: u64 = 1;
+const SLOW_PULSE_TIME_INCR: u64 = 10;
 
 fn main() {
     let manager = battery::Manager::new().unwrap();
 
-    // Assume this is a laptop and there's only one single battery
+    // Assume this is a laptop and there's only one battery
     assert_eq!(manager.batteries().iter().count(), 1);
 
+    // Again, assume a single screen
+    assert_eq!(bb::brightness_devices().count(), 1);
+
     let mut my_battery = manager.batteries().unwrap().nth(0).unwrap().unwrap();
+    let my_screen = bb::brightness_devices().nth(0).unwrap().unwrap();
 
     loop {
         manager.refresh(&mut my_battery).unwrap();
@@ -23,14 +29,29 @@ fn main() {
         {
             wait_time = Duration::from_secs(ONE_SECOND);
 
-            // TODO: brightness blink
+            // Blink
+            for _ in 1..=5 {
+                my_screen.set(5).unwrap();
+                thread::sleep(Duration::from_millis(250));
+                my_screen.set(75).unwrap();
+                thread::sleep(Duration::from_millis(250));
+            }
         } else if my_battery.state() == battery::State::Charging
             && my_battery.state_of_charge().value >= NINETY_PERCENT
             || my_battery.state() == battery::State::Full
         {
             wait_time = Duration::from_secs(ONE_SECOND);
 
-            // TODO: brightness slow pulse
+            // Slow pulse
+            for percentage in (5..=100).filter(|x| x % 5 == 0) {
+                my_screen.set(percentage).unwrap();
+                thread::sleep(Duration::from_millis(SLOW_PULSE_TIME_INCR));
+            }
+
+            for percentage in (5..=100).rev().filter(|x| x % 5 == 0) {
+                my_screen.set(percentage).unwrap();
+                thread::sleep(Duration::from_millis(SLOW_PULSE_TIME_INCR));
+            }
         };
 
         thread::sleep(wait_time);
